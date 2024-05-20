ffc7b290face6d9142c027e15eb5b719430b9f89
Gabriel Almeida <gabriel.brandaoal@gmail.com>
2024-05-20 18:59:48 -0300

    test accelerate

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 
 import numpy as np
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
-try:
-    import torch_xla.distributed.xla_multiprocessing as xmp
-    import torch_xla.distributed.parallel_loader as pl
-    import torch_xla.core.xla_model as xm
-except ImportError:
-    xmp = None
+from accelerate import Accelerator
+import transformers
+from transformers import (
+    AdamW,
+    get_linear_schedule_with_warmup
+)
 
 from settings import hparams
 from project.data.data_module import DataModule
@@ -20,13 +20,17 @@ from project.logger.writer import get_summary_writer
 def create_tqdm_bar(iterable, desc):
     return tqdm(enumerate(iterable),total=len(iterable), ncols=100, desc=desc)
 
-def train_model():
+def train_model(model, hparams=hparams, metric):
+    accelerator = Accelerator()
+
+    name = model._get_name()
+
+    if accelerator.is_main_process:
+        transformers.utils.logging.set_verbosity_info()
+    else:
+        transformers.utils.logging.set_verbosity_error()
+
     data_module = DataModule(hparams)
-
-    naive_model = NaiveModel(hparams=hparams)
-    naive_model.apply(init_weights)
-
-    model = NaiveModel(hparams)
 
     loss_func = nn.BCELoss()
 
@@ -35,80 +39,56 @@ def train_model():
 
     logger = get_summary_writer(model)
 
-    loss_cutoff = len(train_loader) // 10
-    optimizer = torch.optim.Adam(model.parameters(), hparams['learning_rate'])
-
-    if torch.backends.mps.is_available():
-        device = torch.device("mps")
-    else:
-        device = xm.xla_device()
-
-    model.to(device)
+    optimizer = AdamW(params=model.parameters(), lr=hparams['learning_rate'])
 
     model_name = model._get_name()
     epochs = hparams['epochs']
 
-    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(epochs * len(train_dataloader) / 5), gamma=hparams.get('gamma', 0.8))
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer, 
+        num_warmup_steps=100,
+        num_training_steps=len(train_loader) * epochs
+    )
+
+    progress_bar = tqdm(range(epochs * len(train_loader)), disable=not accelerator.m)
 
     for epoch in range(epochs):
         model.train() 
-
         training_loss = []
-        validation_loss = []
-
-        train_loop = create_tqdm_bar(train_loader, desc=f'Training Epoch [{epoch + 1}/{epochs}]')
-        
-        for iter, batch in train_loop:
-            optimizer.zero_grad() 
-
+        for iter, batch in enumerate(train_loader):
             images, labels = batch
-            images, labels = images.to(device), labels.to(device)
 
             pred = model(images) 
             loss = loss_func(pred, labels.float())
             
-            loss.backward()  
+            loss.backward()
+            optimizer.step()
+            scheduler.step() 
 
-            if xmp is not None:
-                xm.optimizer_step(optimizer)
-            else:
-                optimizer.step() 
-            # scheduler.step() 
+            logger.add_scalar(f'model_{name}/train_loss', loss.item(), epoch * len(train_loader) + iter)
 
-            training_loss.append(loss.item())
-            training_loss = training_loss[-loss_cutoff:]
-
-            train_loop.set_postfix(curr_train_loss = "{:.8f}".format(np.mean(training_loss)),
-                                      lr = "{:.8f}".format(optimizer.param_groups[0]['lr'])
-            )
-
-            logger.add_scalar(f'classifier_{model_name}/train_loss', loss.item(), epoch * len(train_loader) + iter)
-        
-        print(f'train epoch {epoch}: average_loss:{np.mean(training_loss):.8f}"')
+            optimizer.zero_grad()
 
         model.eval()
-        valid_loop = create_tqdm_bar(valid_loader, desc=f'Validation Epoch [{epoch + 1}/{hparams["epochs"]}]')
+        validation_loss = []
 
-        with torch.no_grad():
-            iter = 0
-            for iter, batch in valid_loop:
+        for iter, batch in enumerate(valid_loader):
+            with torch.no_grad():
                 images, labels = batch
-
-                images, labels = images.to(device), labels.to(device)
-
                 pred = model(images)
-                loss = loss_func(pred, labels.float())
-                validation_loss.append(loss.item())
 
-                valid_loop.set_postfix(valid_loss = "{:.8f}".format(np.mean(validation_loss)))
+                loss = loss_func(pred, labels.float())
 
                 logger.add_scalar(f'classifier_{model_name}/val_loss', loss.item(), epoch * len(valid_loader) + iter)
-        print(f'valid epoch {epoch}: average_loss:{np.mean(training_loss):.8f}"')
+        
             
 
     return model
 
 if __name__ == '__main__':
+    naive_model = NaiveModel(hparams=hparams)
+    naive_model.apply(init_weights)
+
     train_model()
 
     # train_dataloader = data_module.get_train_dataloader()
