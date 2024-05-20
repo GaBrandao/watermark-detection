9811821dd359a5ce0cbd50b1401451597f970bb9
Gabriel Almeida <gabriel.brandaoal@gmail.com>
2024-05-20 17:58:03 -0300

    remove mp

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 hparams = {
     'learning_rate': 1e-05,
-    'batch_size': 256,
+    'batch_size': 64,
     'batch_norm': False,
-    'epochs': 20,
+    'epochs': 10,
     'num_classes': 2,
     'num_workers': 8,
     'load_method': 'memory',
--- train.py
+++ train.py
@@ -20,7 +20,7 @@ from project.logger.writer import get_summary_writer
 def create_tqdm_bar(iterable, desc):
     return tqdm(enumerate(iterable),total=len(iterable), ncols=100, desc=desc)
 
-def train_model(index):
+def train_model():
     data_module = DataModule(hparams)
 
     naive_model = NaiveModel(hparams=hparams)
@@ -40,8 +40,6 @@ def train_model(index):
 
     if torch.backends.mps.is_available():
         device = torch.device("mps")
-        train_loader = pl.MpDeviceLoader(train_loader, device)
-        valid_loader = pl.MpDeviceLoader(valid_loader, device)
     else:
         device = xm.xla_device()
 
@@ -58,12 +56,12 @@ def train_model(index):
         training_loss = []
         validation_loss = []
 
-        # training_loop = create_tqdm_bar(train_loader, desc=f'Training Epoch [{epoch + 1}/{epochs}]')
-        iter = 0
-        for images, labels in train_loader:# train_iteration, batch in training_loop:
-            iter += 1
+        train_loop = create_tqdm_bar(train_loader, desc=f'Training Epoch [{epoch + 1}/{epochs}]')
+        
+        for iter, batch in train_loop:
             optimizer.zero_grad() 
 
+            images, labels = batch
             images, labels = images.to(device), labels.to(device)
 
             pred = model(images) 
@@ -80,21 +78,21 @@ def train_model(index):
             training_loss.append(loss.item())
             training_loss = training_loss[-loss_cutoff:]
 
-            # training_loop.set_postfix(curr_train_loss = "{:.8f}".format(np.mean(training_loss)),
-            #                           lr = "{:.8f}".format(optimizer.param_groups[0]['lr'])
-            # )
+            train_loop.set_postfix(curr_train_loss = "{:.8f}".format(np.mean(training_loss)),
+                                      lr = "{:.8f}".format(optimizer.param_groups[0]['lr'])
+            )
 
             logger.add_scalar(f'classifier_{model_name}/train_loss', loss.item(), epoch * len(train_loader) + iter)
         
         print(f'train epoch {epoch}: average_loss:{np.mean(training_loss):.8f}"')
 
         model.eval()
-        # val_loop = create_tqdm_bar(valid_loader, desc=f'Validation Epoch [{epoch + 1}/{hparams["epochs"]}]')
+        valid_loop = create_tqdm_bar(valid_loader, desc=f'Validation Epoch [{epoch + 1}/{hparams["epochs"]}]')
 
         with torch.no_grad():
             iter = 0
-            for images, labels in valid_loader:# for val_iteration, batch in val_loop:
-                iter += 1
+            for iter, batch, labels in valid_loop:
+                images, labels = batch
 
                 images, labels = images.to(device), labels.to(device)
 
@@ -102,7 +100,7 @@ def train_model(index):
                 loss = loss_func(pred, labels.float())
                 validation_loss.append(loss.item())
 
-                # val_loop.set_postfix(val_loss = "{:.8f}".format(np.mean(validation_loss)))
+                valid_loop.set_postfix(valid_loss = "{:.8f}".format(np.mean(validation_loss)))
 
                 logger.add_scalar(f'classifier_{model_name}/val_loss', loss.item(), epoch * len(valid_loader) + iter)
         print(f'valid epoch {epoch}: average_loss:{np.mean(training_loss):.8f}"')
@@ -111,10 +109,7 @@ def train_model(index):
     return model
 
 if __name__ == '__main__':
-    if xmp is not None:
-        xmp.spawn(train_model, args=())
-    else:
-        train_model()
+    train_model()
 
     # train_dataloader = data_module.get_train_dataloader()
     # test_dataloader = data_module.get_test_dataloader()
