83298b333c9f54e27f5fb36f8445421d94d77274
Gabriel Almeida <gabriel.brandaoal@gmail.com>
2024-05-21 11:14:34 -0300

    update requirements

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ export PATH="/home/gabrielalmeida/.local/bin:$PATH"
 export PJRT_DEVICE=TPU
 
 pip install torch~=2.3.0 torch_xla[tpu]~=2.3.0 torchvision -f https://storage.googleapis.com/libtpu-releases/index.html
-pip install jupyterlab
 pip install -r requirements.txt
 
 mkdir dataset
